7522f922137f88fac89dcbf7a4c3c5c039c9b7f7
YuusukeHirose-Sun <ysk.dai.817@gmail.com>
2025-05-21 19:22:23 +0900

    git

--- Lesson3-Chapter13_task/kadai-script.js
+++ Lesson3-Chapter13_task/kadai-script.js
@@ -9,45 +9,45 @@ let clickCount = 0;
 button.addEventListener("click",function(){
 clickCount += 1; //クリック回数を１増やす 
 
-// カウントが１の時だけ以下の処理をする
-if (clickCount === 1){
-  button.addEventListener('click', function() {
-  message.textContent = '1回目ですね。まだまだこれからです！';
-  });
-}
-
-  // カウントが2の時だけ以下の処理をする
+  // カウントが１の時だけ以下の処理をする
+  if (clickCount === 1){
+    button.addEventListener('click', function() {
+      message.textContent = '1回目ですね。まだまだこれからです！';
+    });
+  }
+  
+    // カウントが2の時だけ以下の処理をする
   if (clickCount === 2){
     button.addEventListener('click', function() {
-    message.textContent = '2回目です。いいペースです！';
-  });
-} 
-
-// カウントが3の時だけ以下の処理をする
+      message.textContent = '2回目です。いいペースです！';
+    });
+  } 
+  
+    // カウントが3の時だけ以下の処理をする
   if (clickCount === 3){
     button.addEventListener('click', function() {
-    message.textContent = '3回目！半分きました！';
-  });
-}
-
-   // カウントが4の時だけ以下の処理をする
+      message.textContent = '3回目！半分きました！';
+    });
+  }
+  
+     // カウントが4の時だけ以下の処理をする
   if (clickCount === 4){
     button.addEventListener('click', function() {
-    message.textContent = '4回目です。あと少し！';
-  });
-}
-
-  // カウントが5の時だけ以下の処理をする
+      message.textContent = '4回目です。あと少し！';
+    });
+  }
+  
+    // カウントが5の時だけ以下の処理をする
   if (clickCount === 5){
     button.addEventListener('click', function() {
-    message.textContent = '5回目！お疲れ様でした！';
-  });
-}
-
-  // カウントが6の時ボタンを非表示にする
+      message.textContent = '5回目！お疲れ様でした！';
+    });
+  }
+  
+    // カウントが6の時ボタンを非表示にする
   if (clickCount === 6){
     button.addEventListener('click', function() {
-    button.style.display="none";
-  });
-}
+      button.style.display="none";
+    });
+  }
 })
