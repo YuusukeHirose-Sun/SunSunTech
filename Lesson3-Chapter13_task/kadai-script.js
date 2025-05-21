2954b54064cf83ece6edbdecd516197012a7abfa
YuusukeHirose-Sun <ysk.dai.817@gmail.com>
2025-05-21 19:11:41 +0900

    Lessson3_chapter13 課題２

--- Lesson3-Chapter13_task/kadai-script.js
+++ Lesson3-Chapter13_task/kadai-script.js
@@ -2,7 +2,52 @@
 const button = document.getElementById('clickBtn');
 const message = document.getElementById('message');
 
-// ボタンクリック時の処理を追加
-button.addEventListener('click', function() {
-  message.textContent = 'ボタンがクリックされました！';
-});
+// クリック回数をカウント
+let clickCount = 0;
+
+// ボタンをクリックした時の動作を設定
+button.addEventListener("click",function(){
+clickCount += 1; //クリック回数を１増やす 
+
+// カウントが１の時だけ以下の処理をする
+if (clickCount === 1){
+  button.addEventListener('click', function() {
+  message.textContent = '1回目ですね。まだまだこれからです！';
+  });
+}
+
+  // カウントが2の時だけ以下の処理をする
+  if (clickCount === 2){
+    button.addEventListener('click', function() {
+    message.textContent = '2回目です。いいペースです！';
+  });
+} 
+
+// カウントが3の時だけ以下の処理をする
+  if (clickCount === 3){
+    button.addEventListener('click', function() {
+    message.textContent = '3回目！半分きました！';
+  });
+}
+
+   // カウントが4の時だけ以下の処理をする
+  if (clickCount === 4){
+    button.addEventListener('click', function() {
+    message.textContent = '4回目です。あと少し！';
+  });
+}
+
+  // カウントが5の時だけ以下の処理をする
+  if (clickCount === 5){
+    button.addEventListener('click', function() {
+    message.textContent = '5回目！お疲れ様でした！';
+  });
+}
+
+  // カウントが6の時ボタンを非表示にする
+  if (clickCount === 6){
+    button.addEventListener('click', function() {
+    button.style.display="none";
+  });
+}
+})
